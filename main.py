1c3a55edbdf925ce92205d0cf061a19a9c8c42aa
souroyster <valter.oleksandra@gmail.com>
2023-10-17 12:38:51 +0200

    hw10

---
+++ main.py
@@ -0,0 +1,59 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Phone number must be a string")
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if self.find_phone(old_phone) is None:
+            raise ValueError(f"Phone number '{old_phone}' does not exist.")
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            print(f"Contact '{name}' does not exist in the address book.")
+
+
